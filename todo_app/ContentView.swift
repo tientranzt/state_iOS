3f51b9cf59db80035b7b1b244e4176035634d709
tientran <tientran.home@gmail.com>
2020-06-21 07:13:49 +0700

    Initial Commit

---
+++ todo_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo_app
+//
+//  Created by tientran on 6/21/20.
+//  Copyright © 2020 tientran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
